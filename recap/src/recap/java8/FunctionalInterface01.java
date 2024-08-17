836ff8570509d6f01a0c66056ef2ceb202b9f80f
Hasan Kadir Demircan <hkdemircan06@gmail.com>
2024-08-17 14:16:44 +0300

    added functional interface

---
+++ recap/src/recap/java8/FunctionalInterface01.java
@@ -0,0 +1,12 @@
+package recap.java8;
+
+@FunctionalInterface
+interface Car {
+    public boolean equals(Object o);
+    void run();
+    public String toString();
+}
+
+
+public class FunctionalInterface01 {
+}
